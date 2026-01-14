c1356b1bfe7c638a0cea70c34710c64bffe2dae5
Muhamad Yusof <mhduusop@gmail.com>
2026-01-14 10:36:53 +0700

    feat: login screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,17 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  const isAuthenticated = false;
+  return (
+    <Stack>
+      <Stack.Protected guard={isAuthenticated}>
+        <Stack.Screen name="(tabs)/index" options={{ headerShown: false }} />
+      </Stack.Protected>
+
+      <Stack.Protected guard={!isAuthenticated}>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="login" options={{ headerShown: false }} />
+      </Stack.Protected>
+    </Stack>
+  )
 }
---
+++ app/auth/login.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+
+const LoginScreen = () => {
+   const [username, setUsername] = useState('');
+   const [password, setPassword] = useState('');
+   return (
+      <View style={styles.container}>
+         <Text>Username</Text>
+         <TextInput
+            value={username}
+            onChangeText={setUsername}
+            style={styles.input}
+            placeholder="Enter your username"
+         />
+
+         <Text>Password</Text>
+         <TextInput value={password}
+            onChangeText={setPassword}
+            style={styles.input}
+            secureTextEntry
+            placeholder="Enter your password"
+         />
+
+         <Button title="Sign In" onPress={() => { }} />
+      </View>
+   )
+}
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+   container: {
+      flex: 1,
+      justifyContent: 'center',
+      paddingHorizontal: 20,
+   },
+   input: {
+      borderColor: 'gray',
+      borderWidth: 1,
+      marginBottom: 20,
+   },
+});
